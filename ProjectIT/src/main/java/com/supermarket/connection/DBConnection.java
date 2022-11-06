5574b5dd6114adbc1a40ae6b1bd91ba8ae637be3
Jacob <truongthuanhoa23@gmail.com>
2022-11-06 18:20:04 +0700

    add DBConnection & Test

--- ProjectIT/src/main/java/com/supermarket/connection/DBConnection.java
+++ ProjectIT/src/main/java/com/supermarket/connection/DBConnection.java
@@ -1,8 +1,34 @@
 package com.supermarket.connection;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class DBConnection {
+
     //Information local database
-    private String jdbcURL = "jdbc:mysql://localhost:3306/supermarketdb?useSSL=false";
-    private String jdbcUsername = "mirisrodos";
-    private String jdbcPassword = "123456";
+    private final String jdbcURL = "jdbc:mysql://localhost:3306/supermarketdb";
+    private final String jdbcUsername = "mirisrodos";
+    private final String jdbcPassword = "123456";
+
+    public DBConnection() {
+
+    }
+    //Da test lay duoc du lieu roi
+    public Connection getConnection() {
+        Connection cn = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            cn = DriverManager.getConnection(jdbcURL, jdbcUsername, jdbcPassword);
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return cn;
+    }
 }
