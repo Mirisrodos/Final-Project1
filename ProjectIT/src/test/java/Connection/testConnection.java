45a45687057e6eb5d5c15f10dab31a3c95ff08e5
Jacob <truongthuanhoa23@gmail.com>
2022-11-12 23:53:55 +0700

    DAO

--- ProjectIT/src/test/java/Connection/testConnection.java
+++ ProjectIT/src/test/java/Connection/testConnection.java
@@ -6,6 +6,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 
+import com.supermarket.model.dao.UserDAO;
 import com.supermarket.model.entity.Categories;
 import com.supermarket.model.entity.Orders;
 import com.supermarket.model.entity.Orderdetails;
@@ -17,18 +18,15 @@ import com.supermarket.util.HibernateUtils;
 public class testConnection {
 
 	public static void main(String[] args) {
-		Date date = new Date(2002-12-12);
+//		Date date = new Date(2002-12-12);
+//		
+//		Users user = new Users(3, "hoa@gmail.com", "123123", "Truong Thuan Hoa", "HCM", 123123,
+//				date);
+		UserDAO userDao = new UserDAO();
+//		userDao.create(user);
+//		userDao.close();
+		System.out.println(userDao.validate("hoa@gmail.com", "123123"));
 		
-		SessionFactory factory = HibernateUtils.getSessionFactory();
-		Session session = factory.openSession();
-		Transaction transaction = session.beginTransaction();
-		
-		Categories a = session.load(Categories.class, 1);
-		
-		
-		transaction.commit();
-		System.out.println(a.getDecription());
-		System.out.println("Finally");
 	}
 
 }
